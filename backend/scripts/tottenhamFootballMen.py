3e96c4fff8913b39b4ed2402fdd7a7e469f7ca84
Berdan <berdan.a1408@gmail.com>
2025-09-04 16:42:20 +0100

    Render Fix: Incorporating Selenium into render

--- backend/scripts/tottenhamFootballMen.py
+++ backend/scripts/tottenhamFootballMen.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -44,12 +45,19 @@ def formatDateTime(date, day):
 
 def tottenhamFootballMen():
     finalEvents = []
+
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.binary_location = "/usr/bin/chromium"
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+        options=options
+    )
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get('https://www.tottenhamhotspur.com/fixtures/men/')
     driver.refresh()
     
